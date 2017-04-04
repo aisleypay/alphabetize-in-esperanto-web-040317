731ee47affc0ee4c258d1d87176dbb9b07e8fb61
aisleypay <paisley.singh@gmail.com>
2017-04-04 00:11:56 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
-def alphabetize(arr)
-  # code here
+def alphabetize(arr) #array of strings
+  esperanto_alpha= "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  regular_alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+  arr.sort_by! {|string|
+    string.tr(esperanto_alpha, regular_alpha)
+  }
 end
